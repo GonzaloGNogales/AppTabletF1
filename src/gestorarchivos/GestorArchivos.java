a1051f303a5633c63bae4e23914e95378df044ff
IhoFenixMFS <iho-ladamadeltiempo@live.com>
2019-03-26 22:05:00 +0100

    Creación de ventanas y adicción de navegación

---
+++ src/gestorarchivos/GestorArchivos.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package gestorarchivos;
+
+/**
+ *
+ * @author Marina
+ */
+public class GestorArchivos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Inicio ini = new Inicio();
+        ini.setTitle("Inicio");
+        ini.setVisible(true);
+        
+        
+    }
+    
+}
